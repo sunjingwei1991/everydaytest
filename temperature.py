523bb0163cec6358e7b63f58aed2ebd63439ed50
sunjingwei1991 <171382298@qq.com>
2019-06-07 01:04:12 +0800

    2019.6.7test

---
+++ temperature.py
@@ -0,0 +1,6 @@
+fahrenheit = 0
+print("Fahrenheit Celsius")
+while fahrenheit <= 250:
+    celsius = (fahrenheit - 32) / 1.8 # 转换为摄氏度
+    print("{:5d} {:7.2f}".format(fahrenheit , celsius))
+    fahrenheit = fahrenheit + 25
